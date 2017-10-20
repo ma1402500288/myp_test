8ccc41c6945bfd84f95dfe34c9bee68fef6101c2
ma1402500288 <ma.yanpeng@163.com>
2017-10-20 22:29:22 +0800

    hello_world

---
+++ hello_world.cpp
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg936\cocoartf1504\cocoasubrtf830
+{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red52\green52\blue52;\red109\green109\blue109;\red38\green38\blue38;
+\red0\green0\blue117;\red82\green0\blue83;\red16\green121\blue2;\red11\green84\blue83;}
+{\*\expandedcolortbl;;\cssrgb\c26667\c26667\c26667;\cssrgb\c50196\c50196\c50196\c4706;\cssrgb\c20000\c20000\c20000;
+\cssrgb\c0\c0\c53333;\cssrgb\c40000\c0\c40000;\cssrgb\c0\c53333\c0;\cssrgb\c0\c40000\c40000;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+#include<iostream>\cf4 \
+\pard\pardeftab720\partightenfactor0
+\cf5 using\cf4  \cf5 namespace\cf4  \cf6 std\cf4 ;\
+\cf5 int\cf4  main()\
+\{\
+   \cf6 cout\cf4 <<\cf7 "Hello world!"\cf4 <<endl;\
+   \cf5 return\cf4  \cf8 0\cf4 ;\
+\}}
